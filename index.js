be8647b181ae9492ac27ca65504c9cce038c17a7
James <jamespfarrell@gmail.com>
2017-09-21 10:42:38 +0200

    Refactor to remove ASQ dependency

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-var ASQ = require("asynquence");
-
 var audioContext = null;
 
 function getFileFactory( buffer ) {
     return function(done){
         getBufferFile(buffer, done);
-        console.log('getting '+buffer.path);
     };
 }
 
-export function init (passedAudioContext, bufferFiles, callback) {
-
+export function init (passedAudioContext, bufferFiles) {
+    
     return new Promise((resolve, reject) => {
+        
         audioContext = passedAudioContext ? passedAudioContext : new AudioContext();
 
+        let bufferFileArray = [];
         let bufferFileFunctions = [];
         for(let bufferFileName in bufferFiles)
         {
             if (bufferFiles.hasOwnProperty(bufferFileName)){
-                bufferFileFunctions.push(getFileFactory( { name: bufferFileName, path: bufferFiles[bufferFileName] }));
+                bufferFileArray.push(getBufferFilePromise({ name: bufferFileName, path: bufferFiles[bufferFileName] }));
             }
 
         }
 
-        console.log(bufferFileFunctions);
-
-        ASQ("message")
-            .all(...bufferFileFunctions)
-            .val(function(...buffers){
-                resolve(buffers);
-
-            });
+        Promise.all(bufferFileArray).then((buffers) => {
+            resolve(buffers)
+        });
+        
+        
     });
     
 
+};
+
+var getBufferFilePromise = function (buffer) {
+    return new Promise((resolve, reject) => {
+        var request = new XMLHttpRequest();
+        request.open('GET', '/static' + buffer.path, true);
+        request.responseType = 'arraybuffer';
+        request.onload = function () {
+
+            audioContext.decodeAudioData(request.response).then(function (decodedBuffer) {
+
+                resolve({buffer: decodedBuffer, name: buffer.name});
+
+            });
+
+        };
+
+        request.send();
+    });
 };
 
 var getBufferFile = function (buffer, callback) {
@@ -42,8 +57,7 @@ var getBufferFile = function (buffer, callback) {
 
     request.open('GET', '/static'+buffer.path, true);
     request.responseType = 'arraybuffer';
-    //request.responseType = 'json';
-
+    
     request.onload = function () {
 
         audioContext.decodeAudioData(request.response).then(function (decodedBuffer) {
